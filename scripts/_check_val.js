c4981dbc8096c5873b6acd5332908825c30aef66
Trantrongtan2000 <32.trantrongtan.92@gmail.com>
2026-08-20 08:01:05 +0700

    docs: add Gemini AI Studio roadmap, expand merged plan to 5 AIs

--- scripts/_check_val.js
+++
@@ -1,2 +0,0 @@
-const el = document.querySelector('textarea') || document.querySelector('#prompt-textarea') || document.querySelector('[contenteditable="true"]');
-return 'val_len=' + (el.value ? el.value.length : (el.innerText ? el.innerText.length : 0)) + ' tag=' + el.tagName + ' ce=' + el.isContentEditable;
